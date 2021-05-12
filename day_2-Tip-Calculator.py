54aa0f3c8757970c541f711b936b68b64040e735
Nithin M Thomas <58085922+nithin-mt@users.noreply.github.com>
2021-05-12 11:17:15 +0530

    Update day_2-Tip-Calculator.py

--- day_2-Tip-Calculator.py
+++ day_2-Tip-Calculator.py
@@ -8,17 +8,19 @@ bill_amount = input("Enter the total bill amount : ")
 
 percentage = input ("What percentage of the bill do you want to give as tip 10, 12 or 15 : ")
 
+
 billamount = float(bill_amount)
 percent = float(percentage) 
-
 tip = billamount * (percent/100)
 total_amount = billamount + tip
 
+
 no_of_people = input ("How many people are splitting the bill : ")
 
-num_people = float(no_of_people)
 
+num_people = float(no_of_people)
 amount_per_person = total_amount/num_people
 amount_pp =round(amount_per_person, 2)
+
 
 print(f"Each person should pay : {amount_pp}")
